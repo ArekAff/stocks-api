9f184445a94011125f07d38f2a2df485e6b77497
ArekA <arekaffek4@gmail.com>
2023-05-27 12:53:02 +0200

    change name form stock.ts to stock.entity.ts

--- src/stocks/entities/stocks.entity.ts
+++ src/stocks/entities/stocks.entity.ts
@@ -1,17 +1,24 @@
 import { Field, ObjectType, ID } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+@Entity()
 @ObjectType()
-export class Stock{
-
+export class Stock extends BaseEntity {
+    @PrimaryGeneratedColumn()
     @Field(type => ID)
     id: string;
 
+    @Column()
     @Field()
     ticker: string;
 
+    @Column()
     @Field()
     price: number;
 
+    @Column()
     @Field()
     timestamp: Date;
 }
+
+
