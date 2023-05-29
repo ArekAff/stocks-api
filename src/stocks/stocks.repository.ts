26091068c3a564c2c79cf6a6083bce337dbab5a5
ArekA <arekaffek4@gmail.com>
2023-05-29 15:57:54 +0200

    Fixed TypeError: Cannot read property 'getStock' The problem was that I was trying to use the getStock method of the StockRepository i would not be regonized so i rebuild StockRepository so now it a Injectable class and i can use it in the StockService Alos added stockrepository to the stocks.module.ts providers array

--- src/stocks/stocks.repository.ts
+++ src/stocks/stocks.repository.ts
@@ -1,12 +1,17 @@
-import { Repository,EntityRepository, Entity } from "typeorm";
+import { Repository,EntityRepository, DataSource} from "typeorm";
 import { Stock } from "./entities/stocks.entity";
 import { CreateStockDto } from "./dtos/create-stock.dto";
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 
-@EntityRepository(Stock)
+
+
+@Injectable()
 export class StocksRepository extends Repository<Stock> {
 
+    constructor(private dataSource: DataSource) {
+        super(Stock, dataSource.createEntityManager());
+    }
     async getStocks(): Promise<Stock[]> {
         const query = this.createQueryBuilder('stock');
         const stocks = await query.getMany();
@@ -17,13 +22,14 @@ export class StocksRepository extends Repository<Stock> {
         const stock = this.create({
             ticker,
             price,
+            timestamp: new Date(),
         });
 
         try {
             await stock.save();
             return stock;
         } catch (error) {
-            throw new InternalServerErrorException();
+            throw error.message;
         }
     }
 }
